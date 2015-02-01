71e6be52a6b7431b7639c3d589eb6950619b9cd9
rkouere <rkouere@gmail.com>
2015-02-01 17:30:34 +0100

    correction debordement de pile. L'option -m32 marche maintenant

--- dir.c
+++ dir.c
@@ -119,7 +119,7 @@ add_entry(unsigned int idir, unsigned int inumber, const char *basename)
     /* built the entry */
     entry.ent_inumber = inumber;
     strncpy(entry.ent_basename, basename, ENTRYMAXLENGTH);
-    entry.ent_basename[ENTRYMAXLENGTH] = -1;
+    entry.ent_basename[ENTRYMAXLENGTH - 1] = 0;
 
     /* seek to the right position */
     seek2_ifile(fd, ientry * sizeof(struct entry_s));
